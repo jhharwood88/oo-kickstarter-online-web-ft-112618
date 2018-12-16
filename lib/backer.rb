cc7975f1d97cd927e92ec7d6f4ef49985ece00f7
Jonathan Harwood <jhharwood88@gmail.com>
2018-12-16 22:31:25 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,7 +1,11 @@
 require 'pry'
 class Backer
   
+<<<<<<< HEAD
   attr_accessor :name , :backed_projects 
+=======
+  attr_accessor :name , :backed_projects
+>>>>>>> 8f84182050aec1871cc1040479021d286f0182e9
   
   def initialize(name)
     @name = name
@@ -10,8 +14,12 @@ class Backer
   
   def back_project(project)
     @backed_projects << project
+<<<<<<< HEAD
     project.backers << self
     
+=======
+    Project.backers << self
+>>>>>>> 8f84182050aec1871cc1040479021d286f0182e9
   end
   
 end
--- lib/project.rb
+++ lib/project.rb
@@ -9,7 +9,10 @@ class Project
   
   def add_backer(backer)
     @backers << backer
+<<<<<<< HEAD
     backer.backed_projects << self
+=======
+>>>>>>> 8f84182050aec1871cc1040479021d286f0182e9
   end
   
 end
